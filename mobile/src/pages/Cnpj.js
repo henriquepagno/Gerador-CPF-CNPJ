c0d42e431647b5301c671101868948a034166d5f
Henrique Pagno <henriquepagno@gmail.com>
2019-10-19 23:08:18 -0300

    Finished basic functionalities from mobile module

--- mobile/src/pages/Cnpj.js
+++ mobile/src/pages/Cnpj.js
@@ -1,6 +1,33 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useEffect , useState } from 'react';
+import { View, TextInput, Button } from 'react-native';
+import api from '../../services/api';
 
 export default function Cnpj() {
-    return <View/>;
+    const [ cnpj, setCnpj ] = useState('');
+
+    async function buscaCnpj() {
+        const response = await api.get('/cnpj');
+        setCnpj(response.data.cpf);
+    }
+
+    function handleSubmit(event)
+    {
+        buscaCnpj();
+    }
+
+    useEffect(() => {
+        buscaCnpj();
+    }, [ ])
+
+    return (
+        <View>
+            <TextInput
+                className="txt"
+                id="txtCnpj"
+                value={`${cnpj.cnpj ? cnpj.cnpj : 0}`}
+                readOnly={true}
+            ></TextInput>
+            <Button title="Gerar Novo CNPJ" onPress={handleSubmit}> </Button>
+        </View>
+    )
 }
--- mobile/src/pages/CnpjValidator.js
+++ mobile/src/pages/CnpjValidator.js
@@ -1,6 +1,46 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Button } from 'react-native';
+import api from '../../services/api';
 
-export default function CnpjValidator() {
-    return <View/>;
+import ToggleBox from "../../components/ToggleBox";
+import InputMask from "react-input-mask";
+
+export default function CnpjValidator () {
+    const [ cnpj, setCnpj ] = useState('');
+    const [ cnpjValido, setCnpjValido] = useState('');
+
+    async function ValidaCNPJ() {
+        window.toggleBoxComponent.toggleBoxEnable();
+        const response = await api.post('/cnpj', { cnpj });
+        
+        if (response.data.cnpjValidator.resposta){
+            setCnpjValido("válido");
+        }
+        else {
+            setCnpjValido("inválido");
+        }
+
+    }
+
+    function handleChange( event ) {
+        setCnpj(event.target.value);
+        window.toggleBoxComponent.toggleBoxDisable();
+    }
+
+    return (
+        <View>
+            <InputMask
+                className="txt"
+                id="txtCnpj"
+                placeholder="Informe o CNPJ"
+                value={cnpj}
+                onChange={handleChange}
+                mask="99.999.999/9999-99"
+                ></InputMask>
+            <Button title="Validar" onPress={ValidaCNPJ}> </Button>
+            <ToggleBox>
+                <Text className="labelAnswer">O CNJ é <strong className={cnpjValido === "válido" ? "answerTrue" : "answerFalse"}>{cnpjValido}</strong>.</Text>
+            </ToggleBox>
+        </View>
+    )
 }
